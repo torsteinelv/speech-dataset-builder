f7f53d759c12a2b47288583a86bee708007a20d4
torsteinelv <torstein@thorsland.net>
2026-02-10 12:22:44 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,6 @@ export LD_LIBRARY_PATH="/usr/local/lib/cudnn8:${LD_LIBRARY_PATH:-}"
 
 echo "[entrypoint] LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
 
-# Fail tidlig hvis cuDNN8 ikke er synlig
 if ! ldconfig -p | grep -q "libcudnn_ops_infer.so.8"; then
   echo "[entrypoint] ERROR: libcudnn_ops_infer.so.8 ikke funnet via ldconfig."
   echo "[entrypoint] Innhold i /usr/local/lib/cudnn8:"
